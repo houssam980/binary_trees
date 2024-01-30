d2efab52d0faed5fbc8d63e980c80f4453542fe3
houssam980 <houssam.ghmari1243@gmail.com>
2024-01-30 10:24:22 -0800

    tree size

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,41 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_size - return the size of a tree
+ * @tree: tree
+ * Return: tree size
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+size_t size = 0, rght = 0, lft = 0;
+if (tree == NULL)
+{
+return (0);
+}
+else
+{
+lft = binary_tree_size(tree->left);
+rght = binary_tree_size(tree->right);
+size = rght + lft + 1;
+}
+return (size);
+#include "binary_trees.h"
+/**
+   * binary_tree_size - return the size of a tree
+    * @tree: tree
+     * Return: tree size
+      */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+size_t size = 0, rght = 0, lft = 0;
+if (tree == NULL)
+{
+return (0);
+}
+else
+{
+lft = binary_tree_size(tree->left);
+rght = binary_tree_size(tree->right);
+size = rght + lft + 1;
+}
+return (size);
+}
